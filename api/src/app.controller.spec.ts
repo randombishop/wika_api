218b76d1698939762b202062535d414599240998
randombishop <nabil.abdellaoui@expr-research.com>
2022-04-13 10:26:56 -0400

    elastic search test

--- api/src/app.controller.spec.ts
+++ api/src/app.controller.spec.ts
@@ -3,7 +3,10 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { Neo4jService } from './neo4j.service';
 import { ElasticSearchService } from './elastic.service';
+
 const TEST_USER = 'aaaaaaaaaaaaaaa';
+const TEST_QUERY1 = 'test';
+const TEST_QUERY2 = '(test) OR (wika)';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -18,7 +21,7 @@ describe('AppController', () => {
 
     appController = app.get<AppController>(AppController);
     neo4jService = app.get<Neo4jService>(Neo4jService);
-    esService = app.get<ElasticSearchService>(ElasticSearchService);
+    //esService = app.get<ElasticSearchService>(ElasticSearchService);
   });
 
   describe('ping', () => {
@@ -38,6 +41,22 @@ describe('AppController', () => {
     it('should return urls owned by a user', async () => {
       const urls = await appController.listUrlsByOwner({ user: TEST_USER });
       expect(urls.length).toBe(1);
+    });
+  });
+
+  describe('searchUrls', () => {
+    it('should return an UrlSearch instance with numHits>0 and same number of documents', async () => {
+      const result = await appController.searchUrls({ query: TEST_QUERY1 });
+      expect(result.numHits).toBeGreaterThan(0);
+      expect(result.hits.length).toBe(result.numHits);
+    });
+
+    it('should return more results when adding an OR clause', async () => {
+      const result1 = await appController.searchUrls({ query: TEST_QUERY1 });
+      const numHits1 = result1.numHits;
+      const result2 = await appController.searchUrls({ query: TEST_QUERY2 });
+      const numHits2 = result2.numHits;
+      expect(numHits2).toBeGreaterThan(numHits1);
     });
   });
 
